2ac46a899a438e7fe189b9b98d50d39cc98f9013
Elias Chen <elchen0727@gmail.com>
2025-01-22 18:05:01 -0800

    added another stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,101 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "98.css"
+
 import TaskBar from "./components/taskbar";
 
 export default function Home() {
+  const [isFree, setIsFree] = useState<boolean | undefined>(undefined);
   const [isYes, setIsYes] = useState<boolean | undefined>(undefined);
-  // const [progress, setProgress] = useState<number>(0);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setProgress((prev) => {
-  //       if (prev >= 100) {
-  //         clearInterval(interval)
-  //         return 100
-  //       }
-
-  //       return prev + 1;
-  //     })
-  //   }, 30);
-
-  //   return () => clearInterval(interval);
-  // }, []);
+  const [isError, setIsError] = useState<boolean>(false);
 
   return (
       <div className="flex h-screen">
         <div className="flex w-full justify-center items-center">
           <div className="window absolute">
             <div className="title-bar">
-              <div className="title-bar-text">A Question?</div>
+              <div className="title-bar-text">
+                {isYes ? "YAYYYY" : "A Question?"}
+              </div>
               <div className="title-bar-controls">
-                <button aria-label="Close"></button>
+                <button 
+                  aria-label="Close" 
+                  onClick={() => {
+                    setIsFree(false);
+                    setIsYes(false);
+                    setIsError(false);
+                }}>
+                </button>
               </div>
             </div>
             <div className="window-body">
               {
                 isYes ? (
                   <img src="yippee.gif" alt="yippee" />
+                ) : isFree ? (
+                  <div className="flex flex-col space-y-2">
+                    <img src="flowers.gif" alt="flowers" />
+                    <div className="self-center text-lg">Will you be my Valentine?</div>
+                  </div>
                 ) : (
                   <img src="ask.png" alt="cat asking if free" />
                 )
               }
               <div className="flex m-2 justify-center space-x-10">
-                <button
-                  onClick={() => setIsYes(true)}
-                  title={isYes === undefined ? "u know u wanna:)" : isYes ? "" : "pls gimme a chance"}
-                >
-                  Yes
-                </button>
-                <button 
-                  onClick={() => setIsYes(false)}
-                  disabled={isYes}
-                  title={isYes === undefined ? "don't do it" : isYes ? "no take backsies :)" : "WHYYYYYYY"}
-                >
-                  No
-                </button>
+                {
+                  isYes ? (
+                    <div>{"no take backsies :)"}</div>
+                  ) : isFree ? (
+                    <>
+                      <button 
+                        onClick={() => {
+                          setIsYes(true);
+                          setIsError(false);
+                        }}
+                        title={isError ? "PLSSSSS" : "say yes or else"}
+                      >
+                        Yes
+                      </button>
+                      <button
+                        onClick={() => setIsError(true)}
+                        disabled={isError}
+                        title={isError ? "WHYYYYY" : "don't do it"}
+                      >
+                        No
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        onClick={() => {
+                          setIsFree(true);
+                          setIsError(false);
+                        }}
+                        title={isError ? "PLSSSSSS" : "pls gimme a chance"}
+                      >
+                        Yes
+                      </button>
+                      <button
+                        onClick={() => setIsError(true)}
+                        disabled={isError}
+                        title={isError ? "wrong answer" : "dont click this"}
+                      >
+                        No
+                      </button>
+                    </>
+                  )
+                }
               </div>
             </div>
           </div>
           {
-            isYes === false && (
+            isError && (
               <div className="window relative">
                 <div className="title-bar">
                   <div className="title-bar-text">ERROR 404</div>
                   <div className="title-bar-controls">
-                    <button aria-label="Close" onClick={() => setIsYes(undefined)}></button>
+                    <button aria-label="Close" onClick={() => setIsError(false)}></button>
                   </div>
                 </div>
                 <div className="window-body flex justify-center items-center">
